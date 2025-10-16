9d7a9a6676474c90dbec7d13379d78501ee4ec16
Bruno rasq <brunodiasrasquinha@gmail.com>
2025-10-16 19:48:00 -0300

    usando o buffer para otimizar o print do maze no console

--- maze/main.cpp
+++ maze/main.cpp
@@ -6,31 +6,44 @@
 #include <algorithm>
 #include <windows.h>
 #include <cstdlib>
+#include <sstream>
 
 #define PATH "./maze.txt"
-#define TIME 150 // Delay em ms
+#define TIME 15 // Delay em ms
 
 struct Point {
-    int x, y;
+    uint32_t x, y;
 };
 
 std::vector<Point> delta = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
+
 // Verifica se posição é válida
-bool inBound(
-    const std::vector<std::string>& maze,
-    const std::vector<std::vector<bool>>& visiteds,
-    int x, int y
-) {
+bool in_bound(const std::vector<std::string>& maze, uint32_t x, uint32_t y) {
     return (0 <= x && x < (int)maze.size() &&
-            0 <= y && y < (int)maze[0].size() &&
-            maze[x][y] == '.' && !visiteds[x][y]);
+            0 <= y && y < (int)maze[0].size());
 }
+
+
+// ler o arquivo maze.txt onde esta o maze
+std::vector<std::string> load_maze() {
+    std::ifstream file(PATH);
+    if (!file.is_open()) return {};
+
+    std::string line;
+    std::vector<std::string> maze;
+    while (std::getline(file, line)) maze.push_back(line);
+
+    file.close();
+    return maze;
+}
+
 
 // BFS para encontrar caminho
 std::vector<Point> BFS(const std::vector<std::string>& maze, Point start, Point end) {
-    int n = maze.size();
-    int m = maze[0].size();
+
+    const uint32_t n = maze.size();
+    const uint32_t m = maze[0].size();
 
     std::vector<std::vector<bool>> visiteds(n, std::vector<bool>(m, false));
     std::vector<std::vector<Point>> parent(n, std::vector<Point>(m, {-1, -1}));
@@ -46,13 +59,15 @@ std::vector<Point> BFS(const std::vector<std::string>& maze, Point start, Point 
         if (curr.x == end.x && curr.y == end.y) break;
 
         for (auto d : delta) {
-            int nx = curr.x + d.x;
-            int ny = curr.y + d.y;
+            const uint32_t nx = curr.x + d.x;
+            const uint32_t ny = curr.y + d.y;
 
-            if (inBound(maze, visiteds, nx, ny)) {
-                visiteds[nx][ny] = true;
-                parent[nx][ny] = curr;
-                q.push({nx, ny});
+            if (in_bound(maze, nx, ny)) {
+                if(maze[nx][ny] == '.' && !visiteds[nx][ny]){
+                    visiteds[nx][ny] = true;
+                    parent[nx][ny] = curr;
+                    q.push({nx, ny});
+                }
             }
         }
     }
@@ -67,31 +82,35 @@ std::vector<Point> BFS(const std::vector<std::string>& maze, Point start, Point 
     return path;
 }
 
-std::vector<std::string> loadMaze() {
-    std::ifstream file(PATH);
-    if (!file.is_open()) return {};
-
-    std::string line;
-    std::vector<std::string> maze;
-    while (std::getline(file, line)) maze.push_back(line);
-
-    file.close();
-    return maze;
-}
 
 void clear_console() {
     system("cls");  // ou "clear" no Linux
 }
 
+
 void print_maze_state(const std::vector<std::string>& maze) {
+    std::ostringstream buffer;
+
     for (const std::string& line : maze)
-        std::cout << line << "\n";
+        buffer << line << "\n";
+
+    std::cout << buffer.str() << std::endl;
 }
+
 
 void animate_maze(std::vector<std::string>& maze, const std::vector<Point>& path) {
     for (size_t idx = 0; idx < path.size(); idx++) {
         Point curr = path[idx];
-        maze[curr.x][curr.y] = '#';
+        maze[curr.x][curr.y] = 'X';
+
+        for(auto d : delta){
+            const uint32_t nx = d.x + curr.x;
+            const uint32_t ny = d.y + curr.y;
+
+            if(in_bound(maze, nx, ny))
+                if(maze[nx][ny] == '.')
+                    maze[nx][ny] = 'X';   
+        }
 
         clear_console();
         print_maze_state(maze);
@@ -99,8 +118,9 @@ void animate_maze(std::vector<std::string>& maze, const std::vector<Point>& path
     }
 }
 
+
 int main() {
-    std::vector<std::string> maze = loadMaze();
+    std::vector<std::string> maze = load_maze();
     if (maze.empty()) {
         std::cerr << "Erro ao carregar labirinto\n";
         return 1;
