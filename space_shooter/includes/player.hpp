ca08d4bfa2add17161ee669b9e53e80e2e018e4e
Bruno rasq <brunodiasrasquinha@gmail.com>
2025-11-02 23:27:29 -0200

    modificando cabeçalho da estrutura do player

--- space_shooter/includes/player.hpp
+++ space_shooter/includes/player.hpp
@@ -6,18 +6,28 @@ using namespace std;
 
 namespace PlayerHandler {
 
-    struct Spaceship {
-        uint8_t x, y;               // coordenada da nave no space.
-        uint8_t HP, HP_full;        // vida e vida total.
-        uint8_t atkspd;             // velocidade do projetil.
-        uint8_t movspd;             // velocidade (mov a cada X frames.)
+    #define HP_FULL   500
+    #define MOV_SPEED 3
+    #define ATK_SPEED 3
+    #define DIRECTION 1 
+    #define COORD_X   27
+    #define COORD_Y   5
 
-        // para cima     == ^
-        // para baixo    == v
-        // para esquerda == <
-        // para direita  == >
-        char symbol;  
-        string currentDirection;    // direção atual da nave.              
+    struct Spaceship {
+
+        char symbol = '^';                  // cima(^) baixo(v) esquerda(<) direita(>) 
+        uint8_t direction = DIRECTION;      // cima(1) baixo(2) esquerda(3) direita(4)             
+        uint8_t atkspd = ATK_SPEED;         // velocidade do projetil.
+        uint8_t movspd = MOV_SPEED;         // velocidade (mov a cada X frames.)
+        uint8_t HP_full = HP_FULL;          // vida total.
+        uint8_t HP = HP_FULL;               // vida atual.
+        uint8_t x = COORD_X, y = COORD_Y;   // coordenada da nave no space.
+
+        void setCurrentDirection(const uint8_t dir);
+        void SetSymbolByCurrentDirection();
+        void DecrementHP(const uint8_t damage);
+        void setNewCoord(const uint8_t x, const uint8_t y);
+        char getSymbol() const;
     };
 
     class PlayerSpaceship {
---
+++ space_shooter/src/player.cpp
@@ -0,0 +1,8 @@
+#include "../includes/player.hpp"
+
+namespace PlayerHandler {
+
+    void Spaceship::setNewCoord(const uint8_t x, const uint8_t y){}
+
+    PlayerSpaceship::PlayerSpaceship(){};
+}
